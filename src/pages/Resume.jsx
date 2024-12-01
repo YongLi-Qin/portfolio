3fc25b59caae959b085900e198eaaa36559bb82d
YongLiQIN <100742499+YongLi-Qin@users.noreply.github.com>
2024-12-01 17:02:07 +1100

    fix the error of displaying the resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,22 +3,20 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 
-import Particle from '../components/Particle'
-import pdf from "../assets/Resume Matt Qin.pdf"
+import Particle from '../components/Particle';
+import pdf from "../assets/Resume Matt Qin.pdf";
 
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://raw.githubusercontent.com/19sajib/portfolio/main/src/assets/Resume Matt Qin.pdf`
-
+const resumeLink = "https://raw.githubusercontent.com/YongLi-Qin/portfolio/main/src/assets/Resume%20Matt%20Qin.pdf";
 
 const Resume = () => {
   const [width, setWidth] = useState(1200);
 
   useEffect(() => {
-    
     setWidth(window.innerWidth);
   }, []);
 
@@ -38,9 +36,9 @@ const Resume = () => {
           </Button>
         </Row>
 
-        <Row className="resume">
+        <Row className="resume" style={{ justifyContent: "center", maxWidth: "90%", margin: "auto" }}>
           <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={1} scale={width > 1200 ? 1.7 : width > 786 ? 1.2 : 0.8} />
           </Document>
         </Row>
 
@@ -57,7 +55,7 @@ const Resume = () => {
         </Row>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
